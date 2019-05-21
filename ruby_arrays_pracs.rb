26f507598028b097ea2d07ca2f4ea0d92236116b
Thomas Santos <thomas@socialeffort.co>
2019-05-21 16:01:03 -0400

    add a method that concats into array

--- ruby_arrays_pracs.rb
+++ ruby_arrays_pracs.rb
@@ -10,7 +10,27 @@ def double_arr(nums)
         numb_arr << new_num # shovel the value of new_num into empty arr
         i += 1 # iterate into next indice of passed array
     end
+    return numb_arr # the new array
     puts numb_arr # display the result of the new array
 end
 
-puts double_arr([10, 20, 30, 40, 50])
+print double_arr([10, 20, 30, 40, 50])
+puts "next method ---"
+# puts double_arr([10, 20, 30, 40, 50]) == [20, 40, 60, 80, 100]
+
+# write a method that takes in an array of words and retursn a new array where each word has an exclamation point after
+
+def yeller(words) # words is var of an array of strings
+    loud_arr = [] # empty array for strings
+    i = 0 # the indice counter starting @ 0
+    while i < words.length
+        word = words[i] # can't store indice of an int, so keep value of the int "which is string"
+        loud_word = (word + "!") # concat "!" to string store the result new word for each iteration
+        loud_arr << loud_word # shovel result into the array
+        i += 1 # next iteration
+    end
+    return loud_arr
+    puts loud_arr
+end
+
+print yeller(["Hello", "World"])
