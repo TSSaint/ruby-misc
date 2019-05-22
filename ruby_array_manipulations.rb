eff62daa6dbd73016a170a83561b13a57bc043d1
Thomas Santos <thomas@socialeffort.co>
2019-05-21 21:01:01 -0400

    add ruby pracs - array manipulations

---
+++ ruby_array_manipulations.rb
@@ -0,0 +1,21 @@
+# pracs with new array manipulations
+# << adds elements into the end of an array. evaluates to array
+# array.push adds elements to the end. " "
+# array.pop removes last element of the array. evals to the removed only
+# array.unshift adds element to front. evaluates to array
+# array.shift removes first element. evaluates to the removed only
+
+arr = ["vanessa", "candace", "camille", "lubini"]
+
+arr << "lubona"
+# puts arr
+
+arr.push "lubiola"
+# puts arr
+
+# puts arr.pop
+
+# puts arr.unshift("newguy")
+arr.shift
+
+puts arr
