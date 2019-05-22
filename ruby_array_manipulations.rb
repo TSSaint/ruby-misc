fb0e43c5c4c4a2fe4503527a9a2603fd6ed91687
Thomas Santos <thomas@socialeffort.co>
2019-05-21 21:05:47 -0400

    more ruby manips

--- ruby_array_manipulations.rb
+++ ruby_array_manipulations.rb
@@ -18,4 +18,19 @@ arr.push "lubiola"
 # puts arr.unshift("newguy")
 arr.shift
 
-puts arr
+# puts arr
+
+# array/string.index(ele) - evals to the index where it's found
+# array/string.include?(ele) - evaluates to boolean IF ele is found
+# works for both arrays and strings
+
+arr = ["SF", "NY", "LA"]
+puts arr.index("LA") # index 2
+puts arr.index("PA") # nothing, empty space
+
+puts arr.include?("LA") # true
+puts arr.include?("PA") # false
+
+str = "hiya"
+puts str.index("iya") # 1; index 1
+puts str.index("ols") # nothing; empty space
